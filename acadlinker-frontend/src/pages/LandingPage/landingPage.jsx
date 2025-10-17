3b461d72a079f9be2b8715ef8db9f3342795487f
swamini10 <swaminibhandare10@gmail.com>
2025-10-17 23:32:19 +0530

    LandingPage

---
+++ acadlinker-frontend/src/pages/LandingPage/landingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LandingPage = () => {
+    return (
+        <div className='my-4 py-[50px] md:p1-[120px] px-5 md:flex justify-between'>
+            <div className='md:w-[40%]'>
+                <div className='text-4xl mx-auto text-gray-500'>Build your professional brand & network to get ahead in your career</div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage
