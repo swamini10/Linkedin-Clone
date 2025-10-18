1aa807647a3511f2b47df6826f2614acdbfbe62d
swamini10 <swaminibhandare10@gmail.com>
2025-10-18 12:13:24 +0530

    SignUp page

--- acadlinker-frontend/src/App.jsx
+++ acadlinker-frontend/src/App.jsx
@@ -5,15 +5,20 @@ import './App.css'
 import Navbar1 from './components/NavbarV1/navbar1'
 import LandingPage from './pages/LandingPage/landingPage'
 import Footer from './Footer/footer'
+import { Routes, Route } from 'react-router-dom'
+import SignUp from './pages/SignUp/signUp'
 
 function App() {
 
   return (
-      <div className='bg-gray-100 w-[100%] h-[100%] box-border '>
-        <Navbar1/>
-        <LandingPage/>
-        <Footer/>
-      </div>
+    <div className='bg-gray-100 w-[100%] h-[100%] box-border '>
+      <Navbar1 />
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </div>
   )
 }
 
---
+++ acadlinker-frontend/src/pages/SignUp/signUp.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const SignUp = () => {
+    return (
+        <div className="w-full flex flex-col items-center justify-center ">
+            <div className="text-4xl mb-5">Make the first step towards your career</div>
+            <div className="w-[85%] md:w-[28%] shadow-xl rounded-sm box p-10">
+
+                <div className="flex flex-col gap-4">
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="text" className="w-full text-xl border-2 rounded-lg px-5 py-1" placeholder="Email"/>
+                    </div>
+
+                     <div>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="w-full text-xl border-2 rounded-lg px-5 py-1" placeholder="Password"/>
+                    </div>
+
+                     <div>
+                        <label htmlFor="fullname">Full Name</label>
+                        <input type="fullname" className="w-full text-xl border-2 rounded-lg px-5 py-1" placeholder="Full Name"/>
+                    </div>
+
+                    <div className="w-full hover:bg-purple-900 bg-purple-800 text-white py-3 px-4 rounded-xl text-center cursor-pointer  my-2">
+                        Register
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <div className="border-b-1 border-gray-400 w-[45%]"/><div>or</div><div className="border-b-1 border-gray-400 w-[45%] my-6"/>   
+                    </div>
+
+                </div>
+                <div >Google Login Button</div>
+            </div>
+
+            <div className="my-4 mb-10">Already on AcadLinker? <span className="text-blue-800 cursor-pointer">Sign In</span></div>
+            
+        </div>
+    )
+}
+
+export default SignUp
