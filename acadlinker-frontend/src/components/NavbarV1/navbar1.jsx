7be891207ce262cfee67eddc7fc531afa1b653d5
swamini10 <swaminibhandare10@gmail.com>
2025-10-18 08:51:01 +0530

    navbar

--- acadlinker-frontend/src/components/NavbarV1/navbar1.jsx
+++ acadlinker-frontend/src/components/NavbarV1/navbar1.jsx
@@ -12,8 +12,8 @@ const Navbar1 = () => {
             <div className="flex box-border md:gap-4 gap-2 justify-center items-center">
                 {/* <button className="bg-purple-800 text-white py-2 px-4 rounded">Login</button>
                 <button className="bg-transparent border border-purple-800 text-purple-800 py-2 px-4 rounded">Sign Up</button> */}
-               <div className="md:px-4 md:py-2 box-border text-black rounded-3xl text-x1 hover:bg-gray-200 cursor-pointer">Join now</div>
-                <div className="px-4 py-2 box-border border-1 text-purple-800 border-purple-800 rounded-3xl text-x1 hover:bg-purple-200 cursor-pointer">Sign in</div>
+               <div className="md:px-4 md:py-2 box-border text-black rounded-3xl text-2xl hover:bg-gray-200 cursor-pointer">Join now</div>
+               <div className="px-4 py-2 box-border border-1 text-purple-800 border-purple-800 rounded-3xl text-2xl hover:bg-purple-200 cursor-pointer">Sign in</div>
             </div>
         </nav>
     )
