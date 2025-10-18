1308e2a5a5fc762811065f0bc15b9486ed28fe75
swamini10 <swaminibhandare10@gmail.com>
2025-10-18 10:09:53 +0530

    LandingPage

--- acadlinker-frontend/src/pages/LandingPage/landingPage.jsx
+++ acadlinker-frontend/src/pages/LandingPage/landingPage.jsx
@@ -4,7 +4,18 @@ const LandingPage = () => {
     return (
         <div className='my-4 py-[50px] md:p1-[120px] px-5 md:flex justify-between'>
             <div className='md:w-[40%]'>
-                <div className='text-4xl mx-auto text-gray-500'>Build your professional brand & network to get ahead in your career</div>
+                <div className='text-4xl mx-auto text-gray-500 pl-11'>Build your professional brand & network to get ahead in your career</div>
+                <div className='my-3 flex mx-auto mt-[20px] bg-white gap-2 rounded-3xl w-[70%] text-black cursor-pointer'>Google Button</div>
+
+                <div href={'/login'}className='flex mx-auto mt-[20px] py-2 bg-white gap-2 rounded-3xl item-center w-[70%] justify-center text-black hover:bg-gray-100 border-2 cursor-pointer'>Sign in with email</div>
+
+                <div className='mx-auto mb-4 text-sm w-[70%] mt-6'>By clicking Continue to join or sign in, you agree to our <span className='text-blue-800 cursor-pointer hover:underline'>AcadLinker's User Agreement</span>.<span className='text-blue-800 cursor-pointer hover:underline'>Privacy Policy</span> ,and <span className='text-blue-800 cursor-pointer hover:underline'>Cookie Policy</span>.</div>
+
+                <div href={'/signUp'} className='mx-auto text-center mb-4 text-lg w-[70%] mt-4'>New to AcadLinker? <span className='text-blue-800 cursor-pointer hover:underline'>Join now</span></div>
+            
+            </div>
+            <div className='md:w-[50%] h-120'>
+                <img  alt="image" className='w-full h-full 'src="/public/landing-image.png" />
             </div>
         </div>
     )
